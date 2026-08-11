76440f384fe781c09006a40c3533bcbad92676d2
Axiom-an <224730185+Axiom-an@users.noreply.github.com>
2026-08-11 20:51:08 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+Config = {};
+
+Config.serverName = "PARADISE";
+Config.serverSubtitle = "WELCOME TO PARADISE";
+
+Config.social = {
+    discord: "https://discord.gg/QpGFYYf"
+};
+
+Config.audio = {
+    fileName: "music.mp3", 
+    defaultVolume: 0.5     
+};
